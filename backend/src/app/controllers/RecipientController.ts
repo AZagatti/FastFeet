0451fa6354b379bd7883e901287650b918580196
azagatti <andre.zagatti1@gmail.com>
2020-02-09 22:28:01 -0300

    Created update method

--- backend/src/app/controllers/RecipientController.ts
+++ backend/src/app/controllers/RecipientController.ts
@@ -9,25 +9,50 @@ class RecipientController {
       name: Yup.string().required(),
       street: Yup.string().required(),
       number: Yup.number().required(),
-      complement: Yup.string().required(),
+      complement: Yup.string(),
       state: Yup.string().required(),
       city: Yup.string().required(),
-      zip_code: Yup.string().required(),
+      zip_code: Yup.string()
+        .required()
+        .length(8),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { id, name } = await Recipient.create(req.body);
+    const recipient = await Recipient.create(req.body);
 
-    return res.json({
-      id,
-      name,
-    });
+    return res.json(recipient);
   }
 
-  public async update(req: Request, res: Response): Promise<Response> {}
+  public async update(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string().length(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not exists' });
+    }
+
+    const updatedRecipient = await recipient.update(req.body);
+
+    return res.json(updatedRecipient);
+  }
 }
 
 export default new RecipientController();
