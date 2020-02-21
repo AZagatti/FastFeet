af00799c9ff7730401171dadab31f0c8346de973
André Zagatti <andre@allcomsistemas.com.br>
2020-02-21 17:08:00 -0300

    Added delete and update routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -19,5 +19,6 @@ routes.put('/recipients/:id', RecipientController.update);
 routes.get('/deliverymen', DeliverymanController.index);
 routes.post('/deliverymen', DeliverymanController.store);
 routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.delete);
 
 export default routes;
